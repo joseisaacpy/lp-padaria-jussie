2ee33c40bf4ac874b9901677c663a64556774ea5
joseisaactecnotel <joseisaactecnotel@gmail.com>
2025-09-05 15:26:06 -0300

    feat: commit inicial com estrutura de pastas (pages, components, assets)

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+          officia, sed assumenda quo pariatur temporibus consequatur. Cupiditate
+          officia iure dolorum perferendis vero atque earum? Voluptas saepe ut
+          eum at necessitatibus.
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
