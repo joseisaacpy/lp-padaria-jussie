45a2f58ff33d76cfbccd0d9efd5727d0cb41455c
joseisaactecnotel <joseisaactecnotel@gmail.com>
2025-09-05 16:20:23 -0300

    style: adicionar fontes no assets e variáveis no index.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>
+        <h1 className="text-primary">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
           officia, sed assumenda quo pariatur temporibus consequatur. Cupiditate
           officia iure dolorum perferendis vero atque earum? Voluptas saepe ut
